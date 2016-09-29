ce1fb6dc993886657022841a720f1bc0e8ae6aa8
Vasilen Polimenov <vspolimenov@dir.bg>
2016-09-29 12:40:40 +0300

    Add files via upload

---
+++ PractucumOOPProject/GameManager.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractucumOOPProject
+{
+  public static class GameManager
+    {
+        public static void DisplayMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("\t\t\t\t\t\t *** Start ***");
+            Console.WriteLine();
+            Console.WriteLine("\t\t Press 1 to play against Grigorov ");
+            Console.WriteLine("\t\t Press 2 to play against Claus ");
+            Console.WriteLine("\t\t Press 3 for 2 players ");
+            Console.WriteLine("\t\t Press any other key to exit");
+            Console.WriteLine();
+        }
+        public static void DisplayResults(Player firstPlayer, Player secondPlayer)
+        {
+            Console.WriteLine("{0}'s Marked possition: {1}", firstPlayer.Name, firstPlayer.NumberOfMarkedPossition);
+            Console.WriteLine("{0}'s Marked possition: {1}", secondPlayer.Name, secondPlayer.NumberOfMarkedPossition);
+        }
+
+        public static void DisplayResults(Player firstPlayer, ComputerPlayerLogic1 secondPlayer)
+        {
+            Console.WriteLine("{0}'s Marked possition: {1}", firstPlayer.Name, firstPlayer.NumberOfMarkedPossition);
+            Console.WriteLine("{0}'s Marked possition: {1}", secondPlayer.Name, secondPlayer.NumberOfMarkedPossition);
+        }
+
+        public static void DisplayResults(Player firstPlayer, ComputerPlayerLogic2 secondPlayer)
+        {
+            Console.WriteLine("{0}'s Marked possition: {1}", firstPlayer.Name, firstPlayer.NumberOfMarkedPossition);
+            Console.WriteLine("{0}'s Marked possition: {1}", secondPlayer.Name, secondPlayer.NumberOfMarkedPossition);
+        }
+        public static void DisplayResults(ComputerPlayerLogic2 firstPlayer, ComputerPlayerLogic1 secondPlayer)
+        {
+            Console.WriteLine("{0}'s Marked possition: {1}", firstPlayer.Name, firstPlayer.NumberOfMarkedPossition);
+            Console.WriteLine("{0}'s Marked possition: {1}", secondPlayer.Name, secondPlayer.NumberOfMarkedPossition);
+        }
+
+        public static void DisplayTerrain(Terrain currentTerrain)
+        {
+            if (currentTerrain.IsFull)
+            {
+                return;
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int row = 0; row < currentTerrain.SizeOfTheMatrix; row++)
+            {
+                Console.Write("\t\t\t");
+                for (int col = 0; col < currentTerrain.SizeOfTheMatrix; col++)
+                {
+                    Console.Write("  |{0}|  ",currentTerrain.TerrainMatrix[row,col]);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+      public static void GeneratePlayersInTheTerrain(Player firstPlayer, Player secondPlayer, Terrain currentTerrain)
+        {
+            currentTerrain.TerrainMatrix[0, 0] = firstPlayer.PlayerMarker;
+            currentTerrain.TerrainMatrix[currentTerrain.SizeOfTheMatrix - 1, currentTerrain.SizeOfTheMatrix - 1] = secondPlayer.PlayerMarker;
+        }
+        public static void GeneratePlayersInTheTerrain(Player firstPlayer, ComputerPlayerLogic1 secondPlayer, Terrain currentTerrain)
+        {
+            currentTerrain.TerrainMatrix[0, 0] = firstPlayer.PlayerMarker;
+            currentTerrain.TerrainMatrix[currentTerrain.SizeOfTheMatrix - 1, currentTerrain.SizeOfTheMatrix - 1] = secondPlayer.PlayerMarker;
+        }
+        public static void GeneratePlayersInTheTerrain(Player firstPlayer, ComputerPlayerLogic2 secondPlayer, Terrain currentTerrain)
+        {
+            currentTerrain.TerrainMatrix[0, 0] = firstPlayer.PlayerMarker;
+            currentTerrain.TerrainMatrix[currentTerrain.SizeOfTheMatrix - 1, currentTerrain.SizeOfTheMatrix - 1] = secondPlayer.PlayerMarker;
+        }
+        public static void GeneratePlayersInTheTerrain(ComputerPlayerLogic2 firstPlayer, ComputerPlayerLogic1 secondPlayer, Terrain currentTerrain)
+        {
+            currentTerrain.TerrainMatrix[0, 0] = firstPlayer.PlayerMarker;
+            currentTerrain.TerrainMatrix[currentTerrain.SizeOfTheMatrix - 1, currentTerrain.SizeOfTheMatrix - 1] = secondPlayer.PlayerMarker;
+        }
+    }
+}
---
+++ PractucumOOPProject/Terrain.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractucumOOPProject
+{
+  public  class Terrain
+    {
+        private int sizeOfTheMatrix = 3;
+        private int[,] terainMatrix = new int[100, 100];
+        private bool isFull = false;
+
+        public Terrain(int sizeOfTheMatrix)
+        {
+            if (sizeOfTheMatrix >= this.SizeOfTheMatrix)
+            {
+                this.SizeOfTheMatrix = sizeOfTheMatrix;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+       public  int [,] TerrainMatrix
+        {
+            get
+            {
+                return terainMatrix;
+            }
+        }
+        public int SizeOfTheMatrix
+        {
+            get
+            {
+                return sizeOfTheMatrix;
+            }
+
+           private set
+            {
+                sizeOfTheMatrix = value;
+            }
+        }
+
+     public bool IsFull
+        {
+            get
+            {
+                return isFull;
+            }
+
+            set
+            {
+                this.isFull = value;
+            }
+        }
+
+    }
+}
